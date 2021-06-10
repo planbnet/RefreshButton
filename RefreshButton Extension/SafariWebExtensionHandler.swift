8ae90fb07eb609d1622c44e52fe521842efb84c6
Felix Rotthowe <felix@planbnet.org>
2021-06-10 18:50:57 +0200

    Use SFSymbols icon

--- RefreshButton Extension/SafariWebExtensionHandler.swift
+++ RefreshButton Extension/SafariWebExtensionHandler.swift
@@ -9,11 +9,20 @@ import SafariServices
 
 class SafariWebExtensionHandler: SFSafariExtensionHandler {
 
+    var intialized = false
+    
     override func toolbarItemClicked(in window: SFSafariWindow) {
         async {
             await window.activeTab()?.activePage()?.reload()
         }
     }
-
-
+ 
+    override func validateToolbarItem(in window: SFSafariWindow,
+                                      validationHandler: @escaping (Bool, String) -> Void) {
+        guard intialized == false else { return }
+        let image = NSImage(systemSymbolName: "arrow.clockwise", accessibilityDescription: "Refresh")
+        window.getToolbarItem { $0?.setImage(image) }
+        intialized = true
+    }
+    
 }
